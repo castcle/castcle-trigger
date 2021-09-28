ebaa341c58769364b0b42a24cd977a11f5ccf269
Watit Thammarat <tongnakub@hotmail.com>
2021-09-28 11:30:09 +0700

    init

---
+++ posts.py
@@ -0,0 +1,17 @@
+import json
+from mongo_client import mongo_client
+
+db = mongo_client['analytics-db']
+
+
+def handle(event, context):
+    if event == 'ERROR':
+        print('ERROR')
+        return
+
+    print(json.dumps(event, indent=4))
+
+    title = event['detail']['fullDocument']['title']
+    post_id = db.posts.insert_one({'title': title}).inserted_id
+
+    print(f'post id = {post_id}')
