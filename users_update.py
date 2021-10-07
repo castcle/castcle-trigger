1ca688af0fd40a6d37936bfff931d78d2c2ad06f
Navavat Pipatsart <pnastra_castcle@Navavats-MBP.local>
2021-10-07 15:39:06 +0700

    facet into user_update

--- users_update.py
+++ users_update.py
@@ -1,4 +1,4 @@
-# This file is updating content creator statistics among recent contents
+# This file is updating both content creator & hashtag statistics
 import json
 import sys
 from mongo_client import mongo_client
@@ -15,6 +15,238 @@ hashtagStats = analyticsDb['hashtagStats']
 def handle(event, context):
     print(json.dumps(event, indent=4))
 
+    # define cursor
+    cursor = [
+        {
+            # filter for new than 14 days contents
+            '$match': {
+                'createdAt': {
+                    '$gte': (datetime.now() - timedelta(days=14)) 
+                            }
+            }
+        }, {
+            '$facet': {    
+                # updating content creator statistics among recent contents
+                "updateCreatorsCursor": [
+                    {
+                        # summarize by content type
+                        '$group': {
+                            '_id': {
+                                '_id': '$author.id', 
+                                'hashtag': '$hashtags.payload', 
+                                'type': '$type'
+                            }, 
+                            'contentCount': {
+                                '$count': {}
+                            }, 
+                            'likeCount': {
+                                '$sum': '$engagements.like.count'
+                            }, 
+                            'commentCount': {
+                                '$sum': '$engagements.comment.count'
+                            }, 
+                            'recastCount': {
+                                '$sum': '$engagements.recast.count'
+                            }, 
+                            'quoteCount': {
+                                '$sum': '$engagements.quote.count'
+                            }
+                        }
+                    }, {
+                        # summarize by hastag payload (not id)
+                        '$group': {
+                            '_id': {
+                                '_id': '$_id._id', 
+                                'hashtag': '$_id.hashtag'
+                            }, 
+                            'hashtagContentCount': {
+                                '$sum': '$contentCount'
+                            }, 
+                            'hashtagLikeCount': {
+                                '$sum': '$likeCount'
+                            }, 
+                            'hashtagCommentCount': {
+                                '$sum': '$commentCount'
+                            }, 
+                            'hashtagRecastCount': {
+                                '$sum': '$recastCount'
+                            }, 
+                            'hashtagQuoteCount': {
+                                '$sum': '$quoteCount'
+                            }, 
+                            'contentSummary': {
+                                '$push': {
+                                    'type': '$_id.type', 
+                                    'typeCount': '$contentCount', 
+                                    'likeCount': '$likeCount', 
+                                    'commentCount': '$commentCount', 
+                                    'recastCount': '$recastCount', 
+                                    'quoteCount': '$quoteCount'
+                                }
+                            }
+                        }
+                    }, {
+                        # summarize by user (not account)
+                        '$group': {
+                            '_id': '$_id._id', 
+                            'userContentCount': {
+                                '$sum': '$hashtagContentCount'
+                            }, 
+                            'userLikeCount': {
+                                '$sum': '$hashtagLikeCount'
+                            }, 
+                            'userCommentCount': {
+                                '$sum': '$hashtagCommentCount'
+                            }, 
+                            'userRecastCount': {
+                                '$sum': '$hashtagRecastCount'
+                            }, 
+                            'userQuoteCount': {
+                                '$sum': '$hashtagQuoteCount'
+                            }, 
+                            'hastagSummary': {
+                                '$push': {
+                                    'hashtag': '$_id.hashtag', 
+                                    'hashtagContentCount': '$hashtagContentCount', 
+                                    'hashtagLikeCount': '$hashtagLikeCount', 
+                                    'hashtagCommentCount': '$hashtagCommentCount', 
+                                    'hashtagRecastCount': '$hashtagRecastCount', 
+                                    'hashtagQuoteCount': '$hashtagQuoteCount', 
+                                    'contentSummary': '$contentSummary'
+                                }
+                            }
+                        }
+                    }, {
+                        # join w/ 'users' collections for more info.
+                        '$lookup': {
+                            'from': 'users', 
+                            'localField': '_id', 
+                            'foreignField': '_id', 
+                            'as': 'userDetail'
+                        }
+                    }, {
+                        # setting format
+                        '$project': {
+                            '_id': 1, 
+                            'ownerAccount': {
+                                '$first': '$userDetail.ownerAccount'
+                            }, 
+                            'displayId': {
+                                '$first': '$userDetail.displayId'
+                            }, 
+                            'createdAt': {
+                                '$first': '$userDetail.createdAt'
+                            }, 
+                            'visibility': {
+                                '$first': '$userDetail.visibility'
+                            }, 
+                            'followedCount': {
+                                '$first': '$userDetail.followedCount'
+                            }, 
+                            'followerCount': {
+                                '$first': '$userDetail.followerCount'
+                            }, 
+                            'userContentCount': 1, 
+                            'userLikeCount': 1, 
+                            'userCommentCount': 1, 
+                            'userRecastCount': 1, 
+                            'userQuoteCount': 1, 
+                            'hastagSummary': 1,
+                            'testingField': "++++++++++hooray++++++++++"
+                        }
+                    }, {
+                        # upsert to 'userStats' collection
+                        '$merge': {
+                            'into': {
+                                'db': 'analytics-db', 
+                                'coll': 'userStats'
+                            }, 
+                            'on': '_id', 
+                            'whenMatched': 'replace', 
+                            'whenNotMatched': 'insert'
+                        }
+                    }
+                ],
+                # updating hashtag statistics among recent contents
+                "updateHashtagCursor": [
+                {
+                    # deconstruct array hashtags into separate documents
+                    '$unwind': {
+                        'path': '$payload.message', 
+                        'preserveNullAndEmptyArrays': True
+                    }
+                }, {
+                    # summarize by user (not account)
+                    '$group': {
+                        '_id': {
+                            'hashtag': '$payload.message', 
+                            'authorId': '$author.id'
+                        }, 
+                        'contribution': {
+                            '$count': {}
+                        }, 
+                        'createdAt': {
+                            '$min': '$createdAt'
+                        }, 
+                        'updatedAt': {
+                            '$max': '$updatedAt'
+                        }
+                    }
+                }, {
+                    # summarize by hashtag
+                    '$group': {
+                        '_id': '$_id.hashtag', 
+                        'hashtagCount': {
+                            '$count': {}
+                        }, 
+                        'createdAt': {
+                            '$min': '$createdAt'
+                        }, 
+                        'updatedAt': {
+                            '$max': '$updatedAt'
+                        }, 
+                        'contributorsDetail': {
+                            '$push': {
+                                '_id': '$_id.authorId', 
+                                'contribution': '$contribution', 
+                                'updatedAt': '$updatedAt'
+                            }
+                        }
+                    }
+                }, {
+                    # setting format
+                    '$project': {
+                        '_id': 0, 
+                        'hashtag': '$_id', 
+                        'hashtagCount': 1, 
+                        'createdAt': 1, 
+                        'updatedAt': 1, 
+                        'contributorsDetail': 1,
+                        'testingField': "++++++++++hooray++++++++++"
+                    }
+                }, {
+                    # upsert to 'hashtagStats' collection
+                    '$merge': {
+                        'into': {
+                            'db': 'analytics-db', 
+                            'coll': 'hashtagStats'
+                        }, 
+                        'on': '_id', 
+                        'whenMatched': 'replace', 
+                        'whenNotMatched': 'insert'
+                        }
+                    }
+                ]
+            }
+        }
+    ]
 
+    try:
+        # perform aggregation w/ resulting in upsert 'userStats' collection
+        contents.aggregate(updateHashtagcursor)
 
+        # print message on complete aggregation
+        print('this aggregation has completed at', datetime.now())
 
+    except ERROR as error:
+        print("ERROR", error)
