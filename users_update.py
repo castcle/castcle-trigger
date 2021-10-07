d4a79d3218b7dced3225138cd9558c2994ec545a
Navavat Pipatsart <pnastra_castcle@Navavats-MBP.local>
2021-10-07 15:36:17 +0700

    facet into user_update

--- users_update.py
+++ users_update.py
@@ -1,11 +1,20 @@
+# This file is updating content creator statistics among recent contents
 import json
+import sys
 from mongo_client import mongo_client
+from bson.objectid import ObjectId
 from datetime import datetime, timedelta
 
+# setup databases & collections
+appDb = mongo_client['app-db']
 analyticsDb = mongo_client['analytics-db']
-contents = analyticsDb ['testContents']
+contents = appDb['contents']
+userStats = analyticsDb['userStats']
+hashtagStats = analyticsDb['hashtagStats']
 
 def handle(event, context):
     print(json.dumps(event, indent=4))
 
-print(datetime.now())
+
+
+
