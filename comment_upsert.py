3acd0ff073bb51d3e8e18e7a11857fa10f342ac6
Navavat Pipatsart <pnastra_castcle@Navavats-MBP.local>
2021-10-11 15:27:45 +0700

    add contentId

--- comment_upsert.py
+++ comment_upsert.py
@@ -22,100 +22,106 @@ def handle(event, context):
     pattern.flags ^= re.UNICODE
 
     # define cursor
+# define cursor
     cursor = [
-    {
-        # filter for new than 14 days contents
-        '$match': {
-            'createdAt': {
-                '$gte': (datetime.now() - timedelta(days=14)) 
-                }
-        }
-    }, {
-        # extract hashtags => array
-        '$addFields': {
-            'hashtags': {
-                '$regexFindAll': {
-                    'input': '$payload.message', 
-                    'regex': pattern
-                }
+        {
+            # filter for new than 14 days contents
+            '$match': {
+                'createdAt': {
+                    '$gte': (datetime.now() - timedelta(days=14)) 
+                    }
             }
-        }
-    }, {
-        # deconstruct hashtags array => hashtag object
-        '$unwind': {
-            'path': '$hashtags', 
-            'preserveNullAndEmptyArrays': True
-        }
-    }, {
-        # extract hashtag object => field
-        '$addFields': {
-        'hashtag': {
-            '$toLower': '$hashtags.match'
-        }
-    }
-    }, {
-        # summarize by user (not account)
-        '$group': {
-            '_id': {
-                'hashtag': '$hashtag', 
-                'authorId': '$author.id'
-            }, 
-            'contribution': {
-                '$count': {}
-            }, 
-            'createdAt': {
-                '$min': '$createdAt'
-            }, 
-            'updatedAt': {
-                '$max': '$updatedAt'
-            }
-        }
-    }, {
-        # summarize by hashtag
-        '$group': {
-            '_id': '$_id.hashtag', 
-            'hashtagCount': {
-                '$sum': '$contribution'
-            }, 
-            'contributorCount': {'$count': {}},
-            'createdAt': {
-                '$min': '$createdAt'
-            }, 
-            'updatedAt': {
-                '$max': '$updatedAt'
-            }, 
-            'contributorsDetail': {
-                '$push': {
-                    '_id': '$_id.authorId', 
-                    'contribution': '$contribution', 
-                    'updatedAt': '$updatedAt'
+        }, {
+            # extract hashtags => array
+            '$addFields': {
+                'hashtags': {
+                    '$regexFindAll': {
+                        'input': '$payload.message', 
+                        'regex': pattern
+                    }
                 }
             }
+        }, {
+            # deconstruct hashtags array => hashtag object
+            '$unwind': {
+                'path': '$hashtags', 
+                'preserveNullAndEmptyArrays': True
+            }
+        }, {
+            # extract hashtag object => field
+            '$addFields': {
+            'hashtag': {
+                '$toLower': '$hashtags.match'
+                },
+                'contentId': "$_id"
+            }
+        }, {
+            # summarize by user (not account)
+            '$group': {
+                '_id': {
+                    'hashtag': '$hashtag', 
+                    'authorId': '$author.id'
+                }, 
+                'contribution': {
+                    '$count': {}
+                }, 
+                'createdAt': {
+                    '$min': '$createdAt'
+                }, 
+                'updatedAt': {
+                    '$max': '$updatedAt'
+                },
+                'contents': {
+                    '$push': "$contentId"
+                }
+            }
+        }, {
+            # summarize by hashtag
+            '$group': {
+                '_id': '$_id.hashtag', 
+                'hashtagCount': {
+                    '$sum': '$contribution'
+                }, 
+                'contributorCount': {'$count': {}},
+                'createdAt': {
+                    '$min': '$createdAt'
+                }, 
+                'updatedAt': {
+                    '$max': '$updatedAt'
+                }, 
+                'contributorsDetail': {
+                    '$push': {
+                        '_id': '$_id.authorId', 
+                        'contribution': '$contribution', 
+                        'updatedAt': '$updatedAt',
+                        'contents': "$contents"
+                    }
+                }
+            }
+        }, {
+            # setting format
+            '$project': {
+                '_id': 0, 
+                'hashtag': '$_id', 
+                'hashtagCount': 1, 
+                'contributorCount': 1,
+                'createdAt': 1, 
+                'updatedAt': 1, 
+                'contributorsDetail': 1
+            }
+        }, {
+            # upsert to 'hashtagStats' collection
+            '$merge': {
+                'into': {
+                    'db': 'analytics-db', 
+                    'coll': 'hashtagStats'
+                }, 
+                'on': '_id', 
+                'whenMatched': 'replace', 
+                'whenNotMatched': 'insert'
+            }
         }
-    }, {
-        # setting format
-        '$project': {
-            '_id': 0, 
-            'hashtag': '$_id', 
-            'hashtagCount': 1, 
-            'contributorCount': 1,
-            'createdAt': 1, 
-            'updatedAt': 1, 
-            'contributorsDetail': 1
-        }
-    }, {
-        # upsert to 'hashtagStats' collection
-        '$merge': {
-            'into': {
-                'db': 'analytics-db', 
-                'coll': 'hashtagStats'
-            }, 
-            'on': '_id', 
-            'whenMatched': 'replace', 
-            'whenNotMatched': 'insert'
-         }
-    }
-    ]
+        ]
 
     try:
         # perform aggregation w/ resulting in upsert 'hashtagStats' collection
--- users_update.py
+++ users_update.py
@@ -1,5 +1,5 @@
 # This file is not work yet
-# aim to return output for 'feedItem' collection
+# ultimate goal is to return contents/contentId w/ reasons as output for 'feedItem' collection
 import json
 import sys
 from mongo_client import mongo_client
@@ -10,10 +10,34 @@ from datetime import datetime, timedelta
 appDb = mongo_client['app-db']
 analyticsDb = mongo_client['analytics-db']
 contents = appDb['contents']
-userStats = analyticsDb['userStats']
+creatorStats = analyticsDb['creatorStats']
 hashtagStats = analyticsDb['hashtagStats']
 
 def handle(event, context):
     print(json.dumps(event, indent=4))
 
 # define cursor
+## now we have 3 effective collections i.e. 'creatorStats', 'hashtagStat', 'contents'
+# 1. filter only potential related contents as aggregator
+# 2. ordering for obain top potential contents as ranker
+
+    # define cursor
+    cursor = [
+    {
+        # filter for new than 14 days contents
+        '$match': {
+            'createdAt': {
+                '$gte': (datetime.now() - timedelta(days=14)) 
+                }
+        }
+    }
+
+    try:
+        # perform aggregation w/ resulting in upsert 'hashtagStats' collection
+        contents.aggregate(cursor)
+
+        # print message on complete aggregation
+        print('this aggregation has completed at', datetime.now())
+
+    except Exception as error:
+        print("ERROR", error)
