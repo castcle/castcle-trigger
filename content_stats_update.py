5ac620590d4a58190626d506c5eddfd4911454b0
Navavat Pipatsart <pnastra_castcle@Navavats-MBP.local>
2021-10-18 11:11:34 +0700

    try run testing contentStats

--- content_stats_update.py
+++ content_stats_update.py
@@ -1,5 +1,5 @@
-# This file is not work yet
-# ultimate goal is to return contents/contentId w/ reasons as output for 'feedItem' collection
+# This file updates collection 'contentStats' from 'contents'
+## just for testing -> in production is running in runtime only (see aggregator_part_topContents)
 import json
 import sys
 from mongo_client import mongo_client
@@ -7,211 +7,119 @@ from bson.objectid import ObjectId
 from bson import regex
 from datetime import datetime, timedelta
 import re
+import math
 
 # setup databases & collections
 appDb = mongo_client['app-db']
-analyticsDb = mongo_client['analytics-db']
 contents = appDb['contents']
-creatorStats = analyticsDb['creatorStats']
-hashtagStats = analyticsDb['hashtagStats']
-
 
 def handle(event, context):
     print(json.dumps(event, indent=4))
 
-# define cursor
-# now we have 3 effective collections i.e. 'creatorStats', 'hashtagStat', 'contents'
-# 1. filter only potential related contents as aggregator
-# 2. ordering for obain top potential contents as ranker
+    # define content parameters
+    contentDateThreshold = 14
+    halfLifeHours = 24
+    topContentslimit = 100
 
-    # define query parameters
-    hashtagThreshold = 5
-    hashtagDateThreshold = 7
-
-    creatorThreshold = 100
-    creatorDateThreshold = 7
-
-    #################################################################
-    # find top hashtag #
-    #################################################################
     # define cursor
-    hashtagCursor = [
+    cursor = [
         {
-            # filter for hashtags those are updated within specific days
+            # filter for only visible contents
             '$match': {
-                'updatedAt': {
-                    '$gte': (datetime.now() - timedelta(days=hashtagDateThreshold))
-                }
-            }
-        }, {
-            # filter non-hashtag out to prevent bias
-            '$match': {
-                '_id': {
-                    '$ne': ''
-                }
-            }
-        }, {
-            # order by number of occurance
-            '$sort': {
-                'hashtagCount': -1
-            }
-        }, {
-            # slice for top numbers
-            '$limit': hashtagThreshold
-        }, {
-            # deconstruct for grouping
-            '$unwind': {
-                'path': '$contributorsDetail'
-            }
-        }, {
-            # summarize all documents into a single array
-            '$group': {
-                '_id': None,
-                'contents': {
-                    '$push': '$contributorsDetail.contents'
-                }
-            }
-            # project deconstruct all contentId into a sigle document with label reason as topHashtags
-        }, {
-            '$project': {
-                '_id': 'topHashtags',
-                'contentIds': {
-                    '$reduce': {
-                        'input': '$contents',
-                        'initialValue': [],
-                        'in': {
-                            '$concatArrays': [
-                                '$$value', '$$this'
-                            ]
-                        }
-                    }
-                }
-            }
-        }
-    ]
-
-    #################################################################
-    # aggregate contents from found hashtags
-    #################################################################
-    # define cursor
-    creatorCursor = [
-        {
-            # filter for new than specific days contents
-            '$match': {
-                'lastContentAt': {
-                    '$gte': (datetime.now() - timedelta(days=creatorDateThreshold))
-                }
-            }
-        }, {
-            # filter for only available creator users
-            '$match': {
+                'createdAt': {
+                    '$gte': (datetime.utcnow() - timedelta(days=contentDateThreshold)) 
+                },
                 'visibility': 'publish'
             }
         }, {
-            # map count values into fractions
+            # map to calculate content low-level score
             '$project': {
-                '_id': 1,
-                'likedRate': {
-                    '$divide': [
-                        '$creatorLikedCount', '$creatorContentCount'
-                    ]
-                },
-                'commentedRate': {
-                    '$divide': [
-                        '$creatorCommentedCount', '$creatorContentCount'
-                    ]
-                },
-                'recastedRate': {
-                    '$divide': [
-                        '$creatorRecastedCount', '$creatorContentCount'
-                    ]
-                },
-                'quotedRate': {
-                    '$divide': [
-                        '$creatorQuotedCount', '$creatorContentCount'
-                    ]
-                },
-                'followedCount': 1,
-                'lastContentAt': 1,
-                'contentSummary': 1
-            }
-        }, {
-            # order by fractions
-            '$sort': {
-                'quotedRate': -1,
-                'recastedRate': -1,
-                'commentedRate': -1,
-                'likedRate': -1,
-                'lastContentAt': -1
-            }
-        }, {
-            # slice for top creator creator users
-            '$limit': creatorThreshold
-        }, {
-            # deconstruct for grouping
-            '$unwind': {
-                'path': '$contentSummary'
-            }
-        }, {
-            # summarize all documents into a single array
-            '$group': {
-                '_id': None,
-                'contents': {
-                    '$push': '$contentSummary.contents'
-                }
-            }
-        }, {
-            # project deconstruct all contentId into a sigle document with label reason as topCreators
-            '$project': {
-                '_id': 'topCreators',
-                'contentIds': {
-                    '$reduce': {
-                        'input': '$contents',
-                        'initialValue': [],
-                        'in': {
-                            '$concatArrays': [
-                                '$$value', '$$this'
+                ## equation: ageScore = e^(-{\lambda}*t)
+                'aggregator.ageScore': {
+                    '$exp': {
+                        '$multiply': [
+                            {
+                                '$divide': [
+                                    {
+                                        '$subtract': [
+                                            datetime.utcnow(), '$updatedAt'
+                                        ]
+                                    }, 60 * 60 * 1000
+                                ]
+                            }, {
+                                '$divide': [
+                                    {
+                                        '$ln': 2
+                                    }, halfLifeHours
+                                ]
+                            }, -1
+                        ]
+                    }
+                }, 
+                ## equation: engagementScore = {\sigma}_{k}({\beta}_{k}*x_{k})
+                'aggregator.engagementScore': {
+                    '$sum': [
+                        {
+                            '$multiply': [
+                                '$engagements.like.count', 1
+                            ]
+                        }, {
+                            '$multiply': [
+                                '$engagements.comment.count', 1
+                            ]
+                        }, {
+                            '$multiply': [
+                                '$engagements.recast.count', 1
+                            ]
+                        }, {
+                            '$multiply': [
+                                '$engagements.quote.count', 1
                             ]
                         }
-                    }
+                    ]
+                }, 
+                # project for investigation
+                'updatedAt': 1, 
+                'like': '$engagements.like.count', 
+                'comment': '$engagements.comment.count', 
+                'recast': '$engagements.recast.count', 
+                'quote': '$engagements.quote.count'
+            }
+        }, {
+            # 
+            '$addFields': {
+                'score': {
+                    '$multiply': [
+                        {
+                            '$add': [
+                                # add bias = 1
+                                '$aggregator.engagementScore', 1
+                            ]
+                        }, '$aggregator.ageScore'
+                    ]
                 }
+            }
+        }, {
+            # upsert to 'contentStats' collection
+            ## equation: score = ageScore*(engagementScore + 1)*(hastagDiversityScore)
+            '$merge': {
+                'into': {
+                    'db': 'analytics-db', 
+                    'coll': 'contentStats'
+                }, 
+                'on': '_id', 
+                'whenMatched': 'replace', 
+                'whenNotMatched': 'insert'
             }
         }
     ]
 
     try:
-        # top hashtags
-        # aggregate then keep results as array of contentIds
-        topHashtagContents = list(hashtagStats.aggregate(hashtagCursor))[
-            0]['contentIds']
+        # perform aggregation w/ resulting in upsert 'hashtagStats' collection
+        contents.aggregate(cursor)
 
         # print message on complete aggregation
-        print('hashtagStats aggregator has returned',
-              len(topHashtagContents), 'contents')
-        print('hashtagStats aggregation has completed at', datetime.now())
-
-        # top creators
-        # aggregate then keep results as array of contentIds
-        topCreatorContents = list(creatorStats.aggregate(creatorCursor))[
-            0]['contentIds']
-
-        # print message on complete aggregation
-        print('creatorStats aggregator has returned',
-              len(topCreatorContents), 'contents')
-        print('creatorStats aggregation has completed at', datetime.now())
-
-        # aggregatedPool
-        # unique combine aggregated content IDs
-        aggregatedPool = topHashtagContents + \
-            list(set(topCreatorContents) - set(topHashtagContents))
-
-        # print message on complete combining
-        print('aggregatedPool has total', len(topCreatorContents), 'contents')
-
-        # print message on complete implemtation
-        print('this aggregation has completed at', datetime.now())
+        print('this aggregation has completed at', datetime.utcnow())
 
     except Exception as error:
         print("ERROR", error)
-
-    #################################################################
