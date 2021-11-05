0d8b2a75666a552988c4c02e9c7a87a2ee1c6892
atitawat pol-in <atitawat.pol@gmail.com>
2021-11-05 18:18:08 +0700

    testing: content_generator.py

--- modules/personalized_content/personalize_content.py
+++ modules/personalized_content/personalize_content.py
@@ -1,7 +1,9 @@
+import pandas as pd
+
 def load_model_from_mongodb(collection, model_name, account):
-    
+    import pickle
     json_data = {}
-    
+        
     # find user's model artifact
     data = collection.find({
         'account': account,
@@ -15,41 +17,50 @@ def load_model_from_mongodb(collection, model_name, account):
     
     return pickle.loads(pickled_model)
 
-def personalized_content():
+def personalized_content(db,
+                         collection_name: str, 
+                         content_features: str,
+                         user_id):
+    # import libraries
     import pymongo # connect to MongoDB
     from pymongo import MongoClient # client connection to MongoDB
     import sklearn
-    import pandas as pd
     import json
     import xgboost as xgb
     import bson.objectid
-    import pickle
     from datetime import datetime
     from pprint import pprint
     import numpy as np
+    
+     #
+    collection = db[collection_name]
+    content_feature = db[content_features]
 
-    # connect to MongoDB
-    ## define connection URI as role; analytics-admin
-    connectionUri = 'mongodb+srv://analytics-admin:pnYT55BGWwHePK1M@dev-cluster.fg2e5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    # perform loading model
+    xg_reg_load = load_model_from_mongodb(collection=collection,
+                                 account=user_id,
+                                 model_name='xgboost')
+#	model_df = pd.DataFrame(list(.find({ "account" : user_id })))
+    # perform loading features
+    # 
+    features = pd.DataFrame(list(content_feature.find({ "account" : user_id })))
 
-    ## assign client
-    client = pymongo.MongoClient(connectionUri)
+    
 
-    ## assign databases
-    appDb = client['app-db']
-    analyticsDb = client['analytics-db']
+    features = features.rename({'_id':'contentId'},axis = 1)\
+        .drop(['contentId','userId'], axis = 1)
+    result_df = pd.DataFrame(xg_reg_load.predict(features), columns = ['predict'])
+    features_rnm_slced = features[['_id']]\
+         .rename({'_id':'contentId'},axis = 1)\
+        .reset_index(drop = True)
+           
+    result_feature_concat = pd.concat([features_rnm_slced,result_df],axis =1)
+    result_feature_concat['userId'] = user_id
+    result_feature_concat = result_feature_concat\
+         .sort_values(by='predict', ascending=False)
+    
+    result_feature_concat_slice = result_feature_concat.head(100)
+    
+    print(result_feature_concat_slice)
 
-    ## USERS download
-    users = appDb['users']
-
-    # explore schema
-    user = pd.DataFrame(list(users.find()))
-
-    ## creator download
-    contentFeatures = analyticsDb['contentFeatures']
-
-    contentFeatures = pd.DataFrame(list(contentFeatures.find()))
-
-    mlArtifacts = analyticsDb['mlArtifacts']
-
-    return
+    return result_feature_concat_slice
