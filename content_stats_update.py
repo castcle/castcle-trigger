4d0cf59d3ef852bbba85362130294866d722656c
Navavat Pipatsart <pnastra_castcle@navavats-mbp.absolute.co.th>
2021-11-15 16:39:50 +0700

    try deploy content stats update

--- content_stats_update.py
+++ content_stats_update.py
@@ -6,12 +6,6 @@ from mongo_client import mongo_client, ping_mongodb
 from bson.objectid import ObjectId
 from bson import regex
 from datetime import datetime, timedelta
-import math
-
-# setup databases & collections
-appDb = mongo_client['app-db']
-contents = appDb['contents']
-
 
 def handle(event, context):
     if event.get("source") == "serverless-plugin-warmup":
@@ -19,129 +13,22 @@ def handle(event, context):
         print("WarmUp - Lambda is warm!")
         return
 
-    print(json.dumps(event, indent=4))
+    # print(json.dumps(event, indent=4))
+    from modules.update_creator_stats.update_creator_stats import update_creator_stats_main
 
     # define content parameters
+    src_database_name = 'app-db',
+    src_collection_name = 'contents'
     contentDateThreshold = 14
     halfLifeHours = 24
     topContentslimit = 100
 
-    try:
+    update_content_stats_main(src_database_name=src_database_name,
+                              src_collection_name=src_collection_name,
+                              contentDateThreshold=contentDateThreshold,
+                              halfLifeHours=halfLifeHours,
+                              topContentslimit=topContentslimit)
 
-        # define cursor
-        contentStatsCursor = [
-            {
-                # filter for only visible contents
-                '$match': {
-                    'createdAt': {
-                        '$gte': (datetime.utcnow() - timedelta(days=contentDateThreshold))
-                    },
-                    'visibility': 'publish'
-                }
-            }, {
-                # map to calculate content low-level score
-                '$project': {
-                    # equation: ageScore = e^(-{\lambda}*t)
-                    'aggregator.ageScore': {
-                        '$exp': {
-                            '$multiply': [
-                                {
-                                    '$divide': [
-                                        {
-                                            '$subtract': [
-                                                datetime.utcnow(), '$updatedAt'
-                                            ]
-                                        }, 60 * 60 * 1000
-                                    ]
-                                }, {
-                                    '$divide': [
-                                        {
-                                            '$ln': 2
-                                        }, halfLifeHours
-                                    ]
-                                }, -1
-                            ]
-                        }
-                    },
-                    # equation: engagementScore = {\sigma}_{k}({\beta}_{k}*x_{k})
-                    'aggregator.engagementScore': {
-                        '$sum': [
-                            {
-                                '$multiply': [
-                                    '$engagements.like.count', 1
-                                ]
-                            }, {
-                                '$multiply': [
-                                    '$engagements.comment.count', 1
-                                ]
-                            }, {
-                                '$multiply': [
-                                    '$engagements.recast.count', 1
-                                ]
-                            }, {
-                                '$multiply': [
-                                    '$engagements.quote.count', 1
-                                ]
-                            }
-                        ]
-                    },
-                    # project for investigation
-                    # add photo count & message character length
-                    'updatedAt': 1,
-                    'likeCount': '$engagements.like.count',
-                    'commentCount': '$engagements.comment.count',
-                    'recastCount': '$engagements.recast.count',
-                    'quoteCount': '$engagements.quote.count',
-                    'authorId': '$author.id',
-                    'photoCount': {
-                        '$size': {
-                            '$ifNull': [
-                                '$payload.photo.contents', []
-                            ]
-                        }
-                    },
-                    'characterLength': {
-                        '$strLenCP': {
-                            '$ifNull': [
-                                '$payload.message', '-'
-                            ]
-                        }
-                    }
-                }
-            }, {
-                # scoring
-                '$addFields': {
-                    'score': {
-                        '$multiply': [
-                            {
-                                '$add': [
-                                    # add bias = 1
-                                    '$aggregator.engagementScore', 1
-                                ]
-                            }, '$aggregator.ageScore'
-                        ]
-                    }
-                }
-            }, {
-                # upsert to 'contentStats' collection
-                ## equation: score = ageScore*(engagementScore + 1)*(hastagDiversityScore)
-                '$merge': {
-                    'into': {
-                        'db': 'analytics-db',
-                        'coll': 'contentStats'
-                    },
-                    'on': '_id',
-                    'whenMatched': 'replace',
-                    'whenNotMatched': 'insert'
-                }
-            }
-        ]
+    print('update content statistics done')
 
-        # perform aggregation w/ resulting in upsert 'contentStats' collection
-        contents.aggregate(contentStatsCursor)
-
-        # print message on complete aggregation
-        print('this aggregation has completed at', datetime.utcnow())
-
-    except Exception as error:
-        print("ERROR", error)
+    return None
---
+++ modules/update_content_stats/update_content_stats.py
@@ -0,0 +1,133 @@
+import json
+import sys
+from mongo_client import mongo_client, ping_mongodb
+from bson.objectid import ObjectId
+from bson import regex
+from datetime import datetime, timedelta
+import math
+
+def update_content_stats_main(src_database_name=src_database_name,
+                              src_collection_name=src_collection_name,
+                              contentDateThreshold=contentDateThreshold,
+                              halfLifeHours=halfLifeHours,
+                              topContentslimit=topContentslimit):
+
+    try:
+
+        # define cursor
+        contentStatsCursor = [
+            {
+                # filter for only visible contents
+                '$match': {
+                    'createdAt': {
+                        '$gte': (datetime.utcnow() - timedelta(days=contentDateThreshold))
+                    },
+                    'visibility': 'publish'
+                }
+            }, {
+                # map to calculate content low-level score
+                '$project': {
+                    # equation: ageScore = e^(-{\lambda}*t)
+                    'aggregator.ageScore': {
+                        '$exp': {
+                            '$multiply': [
+                                {
+                                    '$divide': [
+                                        {
+                                            '$subtract': [
+                                                datetime.utcnow(), '$updatedAt'
+                                            ]
+                                        }, 60 * 60 * 1000
+                                    ]
+                                }, {
+                                    '$divide': [
+                                        {
+                                            '$ln': 2
+                                        }, halfLifeHours
+                                    ]
+                                }, -1
+                            ]
+                        }
+                    },
+                    # equation: engagementScore = {\sigma}_{k}({\beta}_{k}*x_{k})
+                    'aggregator.engagementScore': {
+                        '$sum': [
+                            {
+                                '$multiply': [
+                                    '$engagements.like.count', 1
+                                ]
+                            }, {
+                                '$multiply': [
+                                    '$engagements.comment.count', 1
+                                ]
+                            }, {
+                                '$multiply': [
+                                    '$engagements.recast.count', 1
+                                ]
+                            }, {
+                                '$multiply': [
+                                    '$engagements.quote.count', 1
+                                ]
+                            }
+                        ]
+                    },
+                    # project for investigation
+                    # add photo count & message character length
+                    'updatedAt': 1,
+                    'likeCount': '$engagements.like.count',
+                    'commentCount': '$engagements.comment.count',
+                    'recastCount': '$engagements.recast.count',
+                    'quoteCount': '$engagements.quote.count',
+                    'authorId': '$author.id',
+                    'photoCount': {
+                        '$size': {
+                            '$ifNull': [
+                                '$payload.photo.contents', []
+                            ]
+                        }
+                    },
+                    'characterLength': {
+                        '$strLenCP': {
+                            '$ifNull': [
+                                '$payload.message', '-'
+                            ]
+                        }
+                    }
+                }
+            }, {
+                # scoring
+                '$addFields': {
+                    'score': {
+                        '$multiply': [
+                            {
+                                '$add': [
+                                    # add bias = 1
+                                    '$aggregator.engagementScore', 1
+                                ]
+                            }, '$aggregator.ageScore'
+                        ]
+                    }
+                }
+            }, {
+                # upsert to 'contentStats' collection
+                ## equation: score = ageScore*(engagementScore + 1)*(hastagDiversityScore)
+                '$merge': {
+                    'into': {
+                        'db': 'analytics-db',
+                        'coll': 'contentStats'
+                    },
+                    'on': '_id',
+                    'whenMatched': 'replace',
+                    'whenNotMatched': 'insert'
+                }
+            }
+        ]
+
+        # perform aggregation w/ resulting in upsert 'contentStats' collection
+        mongo_client[src_database_name][src_collection_name].aggregate(contentStatsCursor)
+
+        # print message on complete aggregation
+        print('this aggregation has completed at', datetime.utcnow())
+
+    except Exception as error:
+        print("ERROR", error)
