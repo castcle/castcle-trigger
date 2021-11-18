a9ad3e92059a9a15d0de11a6c9328f76f63e6af6
atitawat pol-in <atitawat.pol@gmail.com>
2021-11-18 14:56:57 +0700

    fix: clear unused codes

--- user_insert.py
+++ user_insert.py
@@ -1,15 +1,6 @@
-# * personalized_content_test
-# Schedule
 import json
 from typing import Collection
 from mongo_client import mongo_client, ping_mongodb
-
-# setup databases & collections
-analyticsDb = mongo_client['analytics-db']
-ml_artifact = 'mlArtifacts_mocked'
-content_feature = 'contentFeatures'
-#collection = analyticsDb['mlArtifacts_mocked']
-
 
 def handle(event, context):
     if event.get("source") == "serverless-plugin-warmup":
@@ -17,19 +8,9 @@ def handle(event, context):
         print("WarmUp - Lambda is warm!")
         return
 
-    from modules.personalized_content.personalize_content \
-        import personalized_content_main
-    from modules.utils.convert_objectid import convert_objectid
+    
     print(json.dumps(event, indent=4))
     print(event)
-    #! accountid = account
-    user_id = event.get('accountid', None)
-    
-    user_id = convert_objectid(user_id)
-    print('[D]:', user_id, repr(user_id), type(user_id))
-    # db=analyticsDb,collection_name=ml_artifact,content_features=content_feature,user_id=user_id
-    personalized_content_result = personalized_content_main(user_id)
-
     
     return {
         "status": 200
