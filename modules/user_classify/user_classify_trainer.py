d0af4621ab1da51f9fb88706de281390ccf7fe76
hken <atitawat.pol@gmail.com>
2022-03-05 13:13:01 +0700

    test: padding

--- modules/user_classify/user_classify_trainer.py
+++ modules/user_classify/user_classify_trainer.py
@@ -25,6 +25,60 @@ def query_data_to_df(mongo_client,
 
     return df
 
+def transform_data(user_engagement_stats_df: pd.DataFrame, 
+                   topics_df: pd.DataFrame
+                   ) -> pd.DataFrame:
+    """
+    Steps to reproduce the data transformation
+    """
+    def padding_topicId(user_engagement_stats_df, topics_df) -> pd.DataFrame:
+        """
+        padding topics id if topicsId are not full
+        """
+        pad_columns = ['countCommentContentTopic', 'countLikeContentTopic', 
+                'countQuoteContentTopic', 'countRecastContentTopic', 
+                'countReportContentTopic']
+        # Temp dataframe
+        new_user_engagement_stats_df = user_engagement_stats_df[pad_columns]
+        # Validate
+        if pad_columns in user_engagement_stats_df.columns:
+            for countEngagementTypeTopic in pad_columns:
+                new_rows_countEngagementTypeTopic = []
+                for row_userCountEagement in user_engagement_stats_df[countEngagementTypeTopic]:
+                    # case full padding
+                    if len(row_userCountEagement) <= 0:
+                        full_padding_user_count_engagement = dict.fromkeys(topics_df['_id'], 0)
+                    # case partial padding
+                    else:
+                        full_padding_user_count_engagement = {}
+                        existing_topicsId = user_engagement_stats_df\
+                            [countEngagementTypeTopic][row_userCountEagement]\
+                                .keys()
+                        for _id in topics_df['_id']:
+                            if _id not in existing_topicsId:
+                                full_padding_user_count_engagement[_id] = 0
+                            else:
+                                full_padding_user_count_engagement[_id] = \
+                                    user_engagement_stats_df[countEngagementTypeTopic]\
+                                        [row_userCountEagement][_id]
+
+                    # store new temp array
+                    new_rows_countEngagementTypeTopic.append(full_padding_user_count_engagement)
+                # when done making new values, assign new values to temp DataFrame
+                new_user_engagement_stats_df[pad_columns] = new_rows_countEngagementTypeTopic
+
+            return new_user_engagement_stats_df
+
+        # not Valid Case
+        else:
+            raise ValueError(f'Padding columns are missing in user_engagement_stats_df, \
+                Please check: {pad_columns}')
+
+    transformed_user_engagement_stats_df = padding_topicId(
+        user_engagement_stats_df, topics_df)
+
+    return transformed_user_engagement_stats_df
+
 def user_classify_trainer_main(mongo_client):
 
     # 1. get data from main collection
@@ -37,8 +91,8 @@ def user_classify_trainer_main(mongo_client):
         src_database_name='analytics-db', 
         src_collection_name='topics',
         no_id=False)
-    
-    
+
+    # 3. transform data
 
 
     return user_engagement_stats_df, topics_df
