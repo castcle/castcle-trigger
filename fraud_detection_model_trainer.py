7b5f24e60bd9b45c0b7d9180d1787baacc8e245d
Wichayut Loha-udom <wichayut@backyard.in.th>
2022-03-07 21:19:52 +0700

    feat: create fraud_detection_model_trainer.py

---
+++ fraud_detection_model_trainer.py
@@ -0,0 +1,41 @@
+"""
+fraud detection model trainer
+function
+    train a fraud detection model using the features of verified documents from the credentialfeatures
+    and save it to the frauddetectionmlartifacts
+    run if any document from the credentialfeatures is verified (verificationStatus & verifiedAt exist in the document)
+"""
+
+from mongo_client import mongo_client, ping_mongodb
+
+
+def handle(event, context):
+    if event.get("source") == "serverless-plugin-warmup":
+        ping_mongodb()
+        print("WarmUp - Lambda is warm!")
+        return
+
+    print("fraud detection model trainer start")
+
+    try:
+        from modules.fraud_detection_model_training import fraud_detection_model_training_main
+
+        features = [
+            "postReadingTimeAbsSkew",
+            "postReadingTimeAbsKurt",
+            "postReadingTimeNormStd",
+            "postReadingTimeDifferenceNormStd"
+        ]
+        fraud_detection_model_training_main(
+            mongo_client,
+            features,
+            source_db="analytics-db",
+            source_collection="credentialfeatures",
+            target_db="analytics-db",
+            target_collection="frauddetectionmlartifacts",
+            document_limit=10000
+        )
+    except Exception as e:
+        print(f"ERROR: {e}")
+
+    print("fraud detection model trainer end")
